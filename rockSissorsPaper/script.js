ad0152428f080a201e2e181a42b3b9a434456d09
Emilio Vicente <juemvicente@gmail.com>
2022-03-30 00:15:20 -0300

    Great advances. Give some styles, and get te game playing

--- rockSissorsPaper/script.js
+++ rockSissorsPaper/script.js
@@ -9,40 +9,79 @@ let computerPlay = (result) => {
          return result = "paper";
 }};
 
+let tablero = document.querySelector(".result");
+let playerChoice = document.querySelectorAll("button");
+let result = document.createElement("div");
+
+
 //Player Choice
-let playerPlay = (result) => {
-    let choose = window.prompt("Choose your play");
-    return result = choose.toLowerCase();
+let playerPlay = (e) => {
+    let btn = e.currentTarget;
+    let choice = btn.dataset.value;
+    result.textContent = playRound(choice,computerPlay());
+    tablero.appendChild(result);
+    //theWinner();
+   };
+
+playerChoice.forEach(choice => choice.addEventListener("click", playerPlay));
+
+/*
+function theWinner () {
+    if (computerScore == 3) {
+        tablero.textContent = "Ooops, you loose!"
+        computerScore = 0;
+        playerScore = 0;
+        humScore.textContent = playerScore;
+        comScore.textContent = computerScore;
+    } else if (playerScore == 3) {
+        tablero.textContent = "Great! You win!"
+        computerScore = 0;
+        playerScore = 0;
+        humScore.textContent = playerScore;
+        comScore.textContent = computerScore;
+    }
 };
+*/
 
 //Deciding the winner
+
+let computerScore = 0;
+let comScore = document.querySelector(".computerScor");
+
+let playerScore = 0;
+let humScore = document.querySelector(".humanScor");
+
 let playRound = (playerSelec, computerSelec) => {
     let result;
 if (playerSelec == "rock" && computerSelec == "scissors") {
+    playerScore++;
+    humScore.textContent = playerScore;
     return result = "You win, ROCK beats SCISSORS";
 } else if (playerSelec == "rock" && computerSelec == "paper") {
+    computerScore++;
+    comScore.textContent = computerScore;
     return result = "You loose, PAPER beat ROCK";
 } else if (playerSelec == "rock" && computerSelec == "rock") {
     return result = "Tie!";
 }else if (playerSelec == "scissors" && computerSelec == "paper") {
+    playerScore++;
+    humScore.textContent = playerScore;
     return result = "You win, SCISSORS beats PAPER"
 } else if (playerSelec == "scissors" && computerSelec == "rock") {
+    computerScore++;
+    comScore.textContent = computerScore;
     return result = "You loose, ROCK beat SCISSORS";
 } else if (playerSelec == "scissors" && computerSelec == "scissors") {
     return result = "Tie!";
 } else if (playerSelec == "paper" && computerSelec == "rock") {
+    playerScore++;
+    humScore.textContent = playerScore;
     return result = "You win, PAPER beats ROCK";
 } else if (playerSelec == "paper" && computerSelec == "scissors") {
+    computerScore++;
+    comScore.textContent = computerScore;
     return result = "You loose, SCISSORS beat PAPER";
 } else if (playerSelec == "paper" && computerSelec == "paper") {
     return result = "Tie!";
 }
 };
-
-let game = () => {
-    for (let i = 0; i < 5; i++) {
-    console.log(playRound(playerPlay(),computerPlay()));
-    };
-};
-
-game();
